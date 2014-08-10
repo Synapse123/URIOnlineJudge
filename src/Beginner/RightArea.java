0ddc0c5283c5895a2892b9b61d00a189d727f24c
Jasmin Thoener <jasmin-thoener@gmx.at>
2014-08-10 14:51:10 +0200

    2 more Beginner Challenges - Status: Beginner(107/114)

---
+++ src/Beginner/RightArea.java
@@ -0,0 +1,46 @@
+package Beginner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class RightArea {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner s = new Scanner(System.in);
+
+        String op = s.nextLine();
+
+        double sum = 0;
+
+        for(int i = 0; i <= 5; i++){
+
+            for(int j = 0; j < 12-i; j++){
+                s.nextDouble();
+            }
+
+            for(int j = 12-i; j < 12; j++){
+                sum += s.nextDouble();
+            }
+
+        }
+
+        for(int i = 5; i >= 0; i--){
+
+            for(int j = 0; j < 12-i; j++){
+                s.nextDouble();
+            }
+
+            for(int j = 12-i; j < 12; j++){
+                sum += s.nextDouble();
+            }
+
+        }
+
+        System.out.printf("%.1f\n", "S".equals(op) ? sum : sum/30);
+
+    }
+
+}
