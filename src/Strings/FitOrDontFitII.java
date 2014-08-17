dd701bc5210b48763450b80dfd431d720aa8845d
Jasmin Thoener <jasmin-thoener@gmx.at>
2014-08-17 21:11:28 +0200

    2 more Strings Challenges - Status: Strings(10/54)

---
+++ src/Strings/FitOrDontFitII.java
@@ -0,0 +1,50 @@
+package Strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 17/07/14: ranked nr. 7 (java)
+
+public class FitOrDontFitII {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+
+            String[] line = br.readLine().split(" ");
+
+            int aLength = line[0].length();
+            int bLength = line[1].length();
+
+            if(aLength < bLength){
+
+                sb.append("nao ");
+
+            }else{
+
+                for(int k = aLength-1, l = bLength-1; l >= 0; k--, l--) {
+
+                    if(line[0].charAt(k) != line[1].charAt(l)){
+                        sb.append("nao ");
+                        break;
+                    }
+
+                }
+
+            }
+
+            sb.append("encaixa\n");
+
+        }
+
+        System.out.print(sb);
+
+    }
+
+}
---
+++ src/Strings/Justifier.java
@@ -0,0 +1,55 @@
+package Strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 17/07/14: ranked nr. 5 (java)
+
+public class Justifier {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        boolean first = true;
+
+        for(;;){
+
+            int n = Integer.parseInt(br.readLine());
+
+            if(n == 0){
+                break;
+            }
+
+            if(!first){
+                sb.append("\n");
+            }else{
+                first = false;
+            }
+
+            String[] lines = new String[n];
+            int max = 0;
+
+            for(int i = 0; i < n; i++){
+                String line = br.readLine();
+                int length = line.length();
+                lines[i] = line;
+                max = length > max ? length : max;
+            }
+
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < max - lines[i].length(); j++){
+                    sb.append(" ");
+                }
+                sb.append(lines[i] + "\n");
+            }
+
+        }
+
+        System.out.print(sb);
+
+    }
+
+}
